4c767198321c310a6d3b9da57a15187d58cc85a4
KenetGaona115 <aldrinkenet@hotmail.com>
2021-03-09 20:56:41 -0600

    asistencia 2 de marzo

--- README.md
+++
@@ -1,2 +0,0 @@
-# Actividad_clase_7
-Asistencia 
---
+++ adopt-me/routes/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const animalsAPI = require('../Animals/animalsAPI')
+const usersAPI = require('../Users/userAPI')
+
+let finalAnimals = animalsAPI.getArray();
+let userData = usersAPI.getData();
+
+const schema = Joi.object({
+  animalname: Joi.string().min(5).required(),
+  breedname: Joi.string(),
+  speciesname: Joi.string(),
+  animalage: Joi.string().required(),
+  basecolour: Joi.string(),
+  owner: Joi.string()
+
+});
+
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+  res.render('index', { finalAnimals });
+});
+
+router.get('/:id', (req, res) => {
+  const { id, imageUrl } = req.params;
+  const { url } = req.query;
+  const animal = animalsAPI.getAnimalById(id)
+  console.log(animal)
+  res.render('details', { animal: animal, image: url })
+});
+
+router.get('/adopt/:id', (req, res) => {
+  const { id } = req.params;
+  const animal = animalsAPI.getAnimalById(id)
+  res.render('adopt', { animal: animal })
+})
+
+router.post('/owner/:id', function (req, res) {
+  const iduser = req.body.iduser
+  const { id } = req.params;
+  const animal = animalsAPI.getAnimalById(id)
+  animal.owner = userData.find(user => user.id == iduser).fullName;
+  res.render('index', { finalAnimals });
+})
+
+//Creamos animal
+router.post('/animals', function (req, res) {
+  const { animalname, breedname, speciesname, animalage, basecolour, owner } = req.body;
+  const result = schema.validate({ animalname, breedname, speciesname, animalage, basecolour, owner });
+  if (result.error) return res.status(400).send(result.error.details[0].message);
+  const animal = animalsAPI.createAnimal(animalname, breedname, speciesname, animalage, basecolour, owner);
+  res.send(animal);
+});
+
+//Actulizamos animal
+router.put('/animals/:id', function (req, res) {
+  const { id } = req.params
+  const { animalname, breedname, speciesname, animalage, basecolour } = req.body;
+  const result = schema.validate({ animalname, breedname, speciesname, animalage, basecolour })
+  if (result.error) return result.status(400).send(result.error.deails)
+  res.send(animalsAPI.updateAnimal(id, animalname, breedname, speciesname, animalage, basecolour));
+})
+//Eliminamos animal 
+router.delete('/animals/:id', function (req, res) {
+  const { id } = req.params;
+  const animal = animalsAPI.deleteAnimal(id)
+  res.send(animal);
+})
+
+module.exports = router;
